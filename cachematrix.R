a4551ce8ec542625dcc90369a98d0d7b4ac8b05f
jonsca <jonathans1978@hotmail.com>
2014-09-18 06:05:59 -0400

    Added additional comments to the source file and to the individual functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,15 @@
-##makeCacheMatrix prepares a matrix that will be passed  
-## to cacheSolve which will calculate the inverse of
-## the matrix if it hasn't been calculated already,
-## if it has, the cached copy will be returned
-
-
-## Write a short comment describing this function
+## Overview of the two functions (see below for additional comments):
+## makeCacheMatrix prepares a matrix object that will be passed  
+## to cacheSolve. cacheSolve will calculate the inverse of
+## the matrix and store it in the matrix object if it hasn't been 
+## calculated already. If the calculated object exists, 
+## the cached copy will be returned
 
 makeCacheMatrix <- function(x = matrix()) {
+#Creates a matrix object and caches its inverse
+# Arguments: x, a matrix whose inverse will be calculated
+# Returns: a matrix object as a list of functions
+  
   inv <- NULL
   set <- function(y){
     x <<- y
@@ -17,13 +20,23 @@ makeCacheMatrix <- function(x = matrix()) {
   setinv <- function(inver) inv <<- inver 
   getinv <- function() inv
   
+  #Return a list object containing these functions
   list(set = set, get = get, setinv = setinv,getinv = getinv)
   
 }
 
 
-## Write a short comment describing this function
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# Calculates the inverse and stores it in the matrix object or
+# returns the already cached inverse
+#
+# Arguments: x, a matrix object created by makeCacheMatrix
+# (can pass additional arguments if needed)
+#
+# Returns: a matrix that is the inverse of 'x'
+
+  
+
 }
